d2af612ef2fe6f36a3c8b4d9717533b7e98c9dc6
Sandhosh <sandhoshtechie02@gmail.com>
2024-11-25 22:58:00 +0530

    changes

--- sample/msg_pass/test/widget_test.dart
+++ sample/msg_pass/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:msg_pass/main.dart';
+import 'package:msg_pass/upload.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
